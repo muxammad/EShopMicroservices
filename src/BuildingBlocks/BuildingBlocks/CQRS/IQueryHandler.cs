1f65517dd82193529a6be631616ffa3dd70de915
mukha <mukha@JARVIS>
2025-02-27 19:43:13 +0500

    Add BuildingBlocks project and implement CQRS pattern
    - Updated solution file to include "BuildingBlocks" project.
    - Modified launchSettings.json to change application URLs.
    - Created ICommand.cs, ICommandHandler.cs, IQuery.cs, and IQueryHandler.cs for CQRS implementation.
    - Added BuildingBlocks.csproj with .NET 8.0 target and MediatR package reference.

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse> where TResponse : notnull where TQuery : IQuery<TResponse>
+    {
+    }
+}
